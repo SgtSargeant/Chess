846914d11d666c20ff12a8fe30fcf1a4a0df7d27
SgtSargeant <jameshenrysargeant@hotmail.co.uk>
2023-01-17 11:31:44 +0000

    Creating List of Buttons
    Updating the Frame now that the JFrame usage is functional, created a Linked List of buttons for access and addition to the JFrame to reduce line spacing.

--- Frame.java
+++ Frame.java
@@ -1,10 +1,39 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.LinkedList;
 
 public class Frame{
     public Frame(){
-        JPanel pan = new JPanel(new GridLayout(10,10));
 
+        LinkedList<JButton> buttonList = new LinkedList<JButton>();
+
+        for(int i = 0; i < 10; i++){ 
+            
+            for(int j = 0; j < 10; j++){
+                JLabel tempLabel = new JLabel(Integer.toString(i*j));
+                JButton tempButton = new JButton();
+                tempButton.add(tempLabel);
+                buttonList.add(tempButton);
+                System.out.println(tempButton);
+            }
+        } 
+
+
+
+        System.out.println("In here");
+        JFrame frame = new JFrame();
+        JPanel pan = new JPanel();
+        JButton button = new JButton();
+        JLabel lab = new JLabel("Testing");
+        pan.setSize(50,50);
+        frame.setSize(100,100);
+        frame.setVisible(true);
+        button.setSize(40,40);
+       
+        frame.add(lab);
+        button.add(lab);
+        frame.add(button);
+        frame.add(pan);
     
     }
 }
